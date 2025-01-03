1cb1b73a1be0ac7726a176ee2b3439855125d901
Thainã Viadroski <14934264+thainaviadroski@users.noreply.github.com>
2025-01-03 10:40:33 -0300

    feat: basic struct blog

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PostComponent } from './components/post/post.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'post/:id', component: PostComponent }
+];
